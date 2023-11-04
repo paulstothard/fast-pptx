f300059c525c3f7bfb411d2fe880a7d1fcade3cc
Paul Stothard <stothard@ualberta.ca>
2023-11-03 18:27:41 -0600

    Update code snippets and formatting

--- scripts/sample_input/code.bash
+++ scripts/sample_input/code.bash
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-echo "Hello, World!"
+# Define source and backup directories
+SOURCE="/path/to/source"
+BACKUP="/path/to/backup"
+
+# Function to perform file sync using rsync
+sync_files() {
+    rsync -av --delete "$SOURCE" "$BACKUP"
+}
+
+# Log start of the script
+echo "Starting backup: $(date)"
+
+# Call the file sync function
+sync_files
+
+# Check if rsync was successful
+if [ $? -eq 0 ]; then
+    echo "Backup completed successfully."
+else
+    echo "Backup failed with error code $?."
+fi
+
+# Log end of the script
+echo "Backup process finished: $(date)"
--- scripts/sample_input/code.r
+++ scripts/sample_input/code.r
@@ -1,3 +1,20 @@
 #!/usr/bin/env Rscript
 
-cat("Hello, World!\n")
+# Simulate some data
+set.seed(42)
+x <- 1:100
+y <- x * 2 + rnorm(100, mean = 0, sd = 50)
+
+# Create a linear model
+model <- lm(y ~ x)
+
+# Summary of the model
+summary(model)
+
+# Plot the data and the model
+plot(x, y, main = "Linear Regression Model", xlab = "X", ylab = "Y")
+abline(model, col = "red")
+
+# Print the coefficients
+print(coef(model))
+
